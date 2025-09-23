164fbaa5648d6076ec61e9dec6ad521d89291847
yojeero <yojeero@gmail.com>
2025-09-23 06:04:35 +0300

    new

--- .icons/extract tar.xz.cmd
+++
@@ -1,9 +0,0 @@
-
-@echo off
-REM Extract .tar from .tar.xz
-"C:\Program Files\7-Zip\7z.exe" e "*.tar.xz" -ooutput
-
-REM Extract contents of .tar into folder
-for %%f in (output\*.tar) do (
-    "C:\Program Files\7-Zip\7z.exe" x "%%f" -ooutput\extracted
-)
